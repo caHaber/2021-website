8405df476e342e7049d8101b929a342670f21068
Casey Haber <caseyhaber@Caseys-MacBook-Air.local>
2021-12-20 13:58:15 -0800

    Saving

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -2,15 +2,131 @@
 </script>
 
 <main>
-	<p id="name">Casey Haber!</p>
-	<p>Engineer, designer, and visualization specialist. Focused on creating unique experiences that communicate a human perspective.</p>
+  <div id="home">
+    <div className="container-fluid" id="main">
+      <div className="row">
+        <div className="col-md-5 content">
+          <div className="paraT">
+            <img className="logo" />
+            <br />
+            <p className="bio bio0">
+              <!-- {/* My name is Casey Haber. I’m an engineer, designer and visualization specialist. I am focused on creating unique experiences that communicate the human perspective.  I have a long history in design and graphics. During my time at university, I quickly discovered my passion for using visualization as a storytelling medium. With this discovery I switched majors from design to computer science and began integrating technology and design in innovative ways. Currently I work as a Data Visualization Scientist and Research Engineer at Two Six Labs, where I am honing my ability to convey complex information as narrative-driven stories. */} -->
+              Engineer, designer, and visualization specialist. Focused on creating
+              unique experiences that communicate a human perspective.
+              <br />
+            </p>
+          </div>
+        </div>
+
+        <div className="col-md-7 link-div">
+          <div className="iconDiv contact-links col-md-12">
+            <h3>CONTACT</h3>
+
+            <p className="link">
+              <a
+                className="git"
+                href="http://www.github.com/caHaber"
+                target="_blank">github/caHaber</a
+              >
+            </p>
+
+            <p className="link">
+              <a className="git" href="mailto:haber.casey@gmail.com"
+                >haber.casey@gmail.com</a
+              >
+            </p>
+
+            <p className="link">
+              <a
+                className="git"
+                href={process.env.PUBLIC_URL + "/img/CaseyHaber_resume2019.pdf"}
+                target="_blank">Resume / CV</a
+              >
+            </p>
+
+            <p className="link">
+              <a
+                className="git"
+                href="https://twitter.com/case_sqrd"
+                target="_blank">twitter</a
+              >
+            </p>
+          </div>
+
+          <div className="iconDiv recent-links col-md-12">
+            <h3>2018 - 2019</h3>
+
+            <p className="link">
+              <!-- <Link className="git" to="/basketball">NBA Salaries</Link> -->
+            </p>
+
+            <p className="link">
+              <a
+                className="git"
+                href="https://twosixtech.com/choosing-open-source-libraries-and-analyzing-risks/"
+                target="_blank"
+              >
+                VISSOFT '19 (Best short paper)</a
+              >
+            </p>
+            <p className="link">
+              <a
+                className="git"
+                href="https://twosixtech.com/visualizing-programming-behaviors-with-stack-overflow/"
+                target="_blank"
+              >
+                Programming Behaviors Blog Post</a
+              >
+            </p>
+          </div>
+
+          <div className="iconDiv archive-links col-md-12">
+            <h3>2016 - 2017</h3>
+            <p className="link">
+              <a className="git" href="archive/SUYI" target="_blank"
+                >Exploring Academic Outcomes</a
+              >
+            </p>
+
+            <p className="link">
+              <a
+                className="git react"
+                href="archive/ZillowRenderDemo"
+                target="_blank">Housing Render Demo</a
+              >
+            </p>
+
+            <p className="link">
+              <a
+                className="git react"
+                title="Simple React transitioning, filter and search"
+                href="archive/Reactd3"
+                target="_blank">Simple ReactD3 Demo</a
+              >
+            </p>
+
+            <p className="link">
+              <a className="git" href="archive/draw" target="_blank"
+                >SVG Draw!</a
+              >
+            </p>
+
+            <p className="link">
+              <a className="git" href="archive/ReactAnimate" target="_blank"
+                >React Animations</a
+              >
+            </p>
+
+            <p className="link">
+              <a className="git react" href="archive/GitReact" target="_blank"
+                >Github Fetch</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </main>
 
-<style>
-
-	#name {
-		font-weight: 400;
-		font-size: 60px;
-	}
-
-</style>
+<!-- </style> -->
